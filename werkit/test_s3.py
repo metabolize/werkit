7c1447e952c5afbb5ada35fa636e4bdd4f729c87
Paul Melnikow <email@paulmelnikow.com>
2020-11-19 15:36:28 -0500

    Remove broken feature and get 100% coverage on S3

--- werkit/test_s3.py
+++ werkit/test_s3.py
@@ -3,7 +3,7 @@ import uuid
 import boto3
 from botocore.exceptions import ClientError
 import pytest
-from .s3 import temp_file_on_s3
+from .s3 import temp_file_on_s3, temp_file_on_s3_from_string
 
 
 @pytest.fixture
@@ -36,9 +36,8 @@ def test_temp_file_on_s3(example_file, test_bucket):
     ) as key:
         assert key == target_key
         s3_client = boto3.client("s3")
-        assert s3_client.get_object(Bucket=test_bucket, Key=target_key)[
-            "Body"
-        ].read() == bytes(EXAMPLE_CONTENTS, "utf-8")
+        response = s3_client.get_object(Bucket=test_bucket, Key=target_key)
+        assert response["Body"].read() == bytes(EXAMPLE_CONTENTS, "utf-8")
 
     # Make sure the key is removed after the context handler is closed
     with pytest.raises(ClientError, match=r"Not Found$"):
@@ -50,4 +49,27 @@ def test_temp_file_on_s3_with_implicit_key(example_file, test_bucket):
         local_path=example_file, bucket=test_bucket, verbose=True
     ) as key:
         assert key.startswith("example_")
+        assert key.endswith(".txt")
+
+
+def test_temp_file_on_s3_from_string(test_bucket):
+    target_key = f"test_{uuid.uuid4().hex}.txt"
+
+    with temp_file_on_s3_from_string(
+        contents=EXAMPLE_CONTENTS, bucket=test_bucket, key=target_key, verbose=True
+    ) as key:
+        assert key == target_key
+        s3_client = boto3.client("s3")
+        response = s3_client.get_object(Bucket=test_bucket, Key=target_key)
+        assert response["Body"].read() == bytes(EXAMPLE_CONTENTS, "utf-8")
+
+    # Make sure the key is removed after the context handler is closed
+    with pytest.raises(ClientError, match=r"Not Found$"):
+        s3_client.head_object(Bucket=test_bucket, Key=target_key)
+
+
+def test_temp_file_on_s3_from_string_with_implicit_key(test_bucket):
+    with temp_file_on_s3_from_string(
+        contents=EXAMPLE_CONTENTS, bucket=test_bucket, extension=".txt", verbose=True
+    ) as key:
         assert key.endswith(".txt")
