8baf25d00f94c0453d5f374cdeebea4c46d8edd2
Paul Melnikow <email@paulmelnikow.com>
2020-11-19 10:31:36 -0500

    Improve S3 temp file utilities
    - Add second context handler `werkit.s3.temp_file_on_s3_from_string()` .
    - Optionally return the eTag.

--- werkit/s3.py
+++ werkit/s3.py
@@ -5,9 +5,10 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def temp_file_on_s3(local_path, bucket, key=None, verbose=False):
+def temp_file_on_s3(local_path, bucket, key=None, verbose=False, ret_etag=False):
     """
-    Copy the given path to S3. Delete the file from S3 when the block exits.
+    Copy the given path to S3. Delete the object from S3 when the block
+    exits.
     """
     import boto3
 
@@ -23,10 +24,43 @@ def temp_file_on_s3(local_path, bucket, key=None, verbose=False):
 
     file_on_s3 = f"s3://{bucket}/{key}"
     pif(f"Uploading {local_path} to {file_on_s3}")
-    s3_client.upload_file(Filename=local_path, Bucket=bucket, Key=key)
+    response = s3_client.upload_file(Filename=local_path, Bucket=bucket, Key=key)
 
     try:
-        yield key
+        yield (key, response["ETag"]) if ret_etag else key
+    finally:
+        pif(f"Removing {file_on_s3}")
+        s3_client.delete_object(Bucket=bucket, Key=key)
+
+
+@contextmanager
+def temp_file_on_s3_from_string(
+    contents, bucket, key=None, extension=None, verbose=False, ret_etag=False
+):
+    """
+    Write the given contents to S3. Delete the object from S3 when the block
+    exits.
+    """
+    import boto3
+
+    def pif(x):
+        if verbose:
+            print(x, file=sys.stderr)
+
+    if key is None:
+        key = f"{uuid.uuid4().hex}{extension}"
+
+    s3_client = boto3.client("s3")
+
+    file_on_s3 = f"s3://{bucket}/{key}"
+    pif(f"Writing to {file_on_s3}")
+
+    response = s3_client.put_object(
+        Bucket=bucket, Key=key, Body=contents.encode("utf-8")
+    )
+
+    try:
+        yield (key, response["ETag"]) if ret_etag else key
     finally:
         pif(f"Removing {file_on_s3}")
         s3_client.delete_object(Bucket=bucket, Key=key)
