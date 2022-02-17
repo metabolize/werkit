6449dc8bc45b9422451756fd80dd55982e9fe69e
Paul Melnikow <email@paulmelnikow.com>
2022-02-17 10:04:31 -0500

    Renames

--- werkit/compute/graph/test_binding.py
+++ werkit/compute/graph/test_binding.py
@@ -2,19 +2,19 @@ import pytest
 from .testing_examples import MyComputeProcess
 
 
-def test_bound_can_access_input_using_property_access():
+def test_bound_property_for_input():
     compute_process = MyComputeProcess()
     compute_process.state_manager.set(a=1)
     assert compute_process.a == 1
 
 
-def test_bound_raises_exception_for_unset_input():
+def test_bound_property_raises_exception_for_unset_input():
     compute_process = MyComputeProcess()
     with pytest.raises(KeyError, match="Input has not been set: a"):
         compute_process.a
 
 
-def test_bound_can_access_node_using_property_access():
+def test_bound_property_for_inner_nodes():
     compute_process = MyComputeProcess()
     compute_process.state_manager.set(a=1)
     assert compute_process.i == 1
