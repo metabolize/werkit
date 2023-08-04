3dabbae09b944543bc63078b377870077d29d151
Jacob Beard <jake@minnow.io>
2023-08-04 10:07:25 -0600

    Try to create target subdirectories

--- werkit/aws_lambda/build.py
+++ werkit/aws_lambda/build.py
@@ -68,6 +68,9 @@ def collect_zipfile_contents(
     lib_files: list[str] = [],
     verbose: bool = False,
 ) -> None:
+    from os.path import dirname, join
+    from pathlib import Path
+
     def pif(x: str) -> None:
         if verbose:
             print(x, file=sys.stderr)
@@ -97,6 +100,8 @@ def collect_zipfile_contents(
 
     for src_file in src_files:
         target = os.path.join(target_dir, src_file)
+        Path(dirname(target)).mkdir(parents=True, exist_ok=True)
+
         pif(f"Copying {src_file} to {target}")
         shutil.copyfile(src_file, target)
 
